f03a9657e772bc3d93e3663d62dc50ed63b63a82
abhishekbhakat <abhishek.bhakat@hotmail.com>
2023-05-11 10:50:35 +0000

    improved delete script for speed

--- delete.py
+++ delete.py
@@ -18,22 +18,50 @@ def delete_from_cache(tgt_folder):
 if not os.path.exists(".cache"):
     print("Cache file not found!")
     exit()
-tgt_folder = input("Enter the project to delete: ")
-tgt_path = os.path.join(os.getcwd(), tgt_folder)
+
+#list all the projects
+with open(".cache", "r") as f:
+    airflows = json.load(f)
+    if len(airflows) == 0:
+        print("No projects found!")
+        exit()
+    print("Projects found:")
+    for id,airflow in enumerate(airflows):
+        print(f"{id+1}. {airflow}")
+print("0. Exit")
+#ask for the project to delete
+choice = input("Enter the project to delete [0]: ")
+if choice in ["0", ""]:
+    exit()
+try:
+    choice = int(choice)
+except ValueError:
+    print("Invalid choice!")
+    exit()
+if choice < 0 or choice > len(airflows):
+    print("Invalid choice!")
+    exit()
+
+project = list(airflows.keys())[choice-1]
+print(f"Deleting project: {project}")
+
+
+# tgt_folder = input("Enter the project to delete: ")
+tgt_path = os.path.join(os.getcwd(), project)
 #check if the project exists
 if not os.path.exists(tgt_path):
     print("Path not found!")
-    delete_from_cache(tgt_folder)
+    delete_from_cache(project)
     exit()
 #if the tgt_folder is not a key in cache print not a project folder and exit
 with open(".cache", "r") as f:
     airflows = json.load(f)
-    if tgt_folder not in airflows:
+    if project not in airflows:
         print("Not a project folder!")
         exit()
 print(f"Checking project: {tgt_path}")
 #compose down the project first
 os.system(f"docker-compose -f {tgt_path}/docker-compose.yaml down")
-delete_from_cache(tgt_folder)
+delete_from_cache(project)
 #delete the project folder
 shutil.rmtree(tgt_path)
